b6b88546146be2c840852aaf4705ce7897624120
Brandon <brandonmoreles7@gmail.com>
2023-07-31 00:37:05 -0600

    ya

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Home from "../src/components/Home.js"
-import Invitacion from "../src/components/Invitacion.js"
-import { createHashRouter, RouterProvider } from 'react-router-dom'; // Cambio de BrowserRouter a HashRouter
-const router= createHashRouter[
-   { path:'/',
-    element:Home},
-    {path:"/invitacion",
-  element:Invitacion}
-]
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <RouterProvider router={router}/>
-);
+import Home from "../src/components/Home.js";
+import Invitacion from "../src/components/Invitacion.js";
+import { HashRouter, RouterProvider } from 'react-router-dom';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const routes = [
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: "/invitacion",
+    element: <Invitacion />
+  }
+];
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <HashRouter>
+    <RouterProvider routes={routes} />
+  </HashRouter>
+);
